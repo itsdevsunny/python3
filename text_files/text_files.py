2c703922c63de0f850829dc211b6fe72b3cb17c5
Your Name <you@example.com>
2018-12-31 15:36:10 +0530

    git text_files add working fine

---
+++ text_files/text_files.py
@@ -0,0 +1,15 @@
+file_path = ""
+file_name = ""
+
+file_log = "error.log"
+
+content = "This is test content."
+
+error_log = "File name not defined"
+
+if file_name != "":
+	with open(file_path+file_name, "w") as file:
+		print(content, file=file)
+else:
+	with open(file_path+file_log, "a") as file:
+		print(error_log, file=file)
