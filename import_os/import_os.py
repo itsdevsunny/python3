5270122f46136200e920b03e24688d075ba1a4ba
Your Name <you@example.com>
2019-01-15 17:57:56 +0530

    OOP Practice Done

--- import_os/import_os.py
+++ import_os/import_os.py
@@ -2,4 +2,32 @@ import os
 
 cwd = os.getcwd()
 
+# dire = cwd.split("")
+
+# for di in dire:
+# 	print(di)
+
+# os.mkdir(cwd+"/test")
+
+# os.chdir()
+
+# os.mkdir("/test2") 
+
+# os.mkdir(cwd+"/test/test3")
+
+# os.makedirs(cwd+"/test/test3/test4/test5")
+
+listd = os.listdir()
+
 print(cwd)
+
+# os.rmdir("test/test3/test4")
+# os.removedirs("test/test3/test4")
+
+print(listd)
+
+# for li in listd:
+# 	print(li)
+# 	li_si = li.split(".")
+# 	for lis in li_si:
+# 		print(lis)
---
+++ oop/prac_oop_class.py
@@ -0,0 +1,138 @@
+# class MyAClass:
+
+# 	def __init__(self, *args):
+# 		self.dataA = args
+		# self.dataB = kwargs
+
+	# def print_data(self):
+	# 	print(self.dataA)
+
+	# def print__data(self):
+	# 	print(self.dataB)
+
+
+# class1 = MyAClass("test", "test", "test")
+
+# class1.print_data()
+
+# class1.print__data()
+
+# class MyBClass:
+
+# 	def __init__(self, **kwargs):
+# 		self.dataA = kwargs
+		# self.dataA = kwargs["test"]
+		# self.dataB = kwargs
+
+# 	def print_data(self):
+# 		print(self.dataA)
+
+# data = {"test":"data", "test2":"data2", "test3":"data3"}
+
+# class2 = MyBClass(**{"test":"data"})
+
+# class2 = MyBClass(**data)
+
+# class2.print_data()
+
+
+
+# class NewAClass(MyAClass):
+
+# 	# override test
+# 	def print_data(self):
+# 		print("this is _data override")
+
+# 	def print__data(self):
+# 		super().print__data()
+# 		# print(dataa)
+# 		print("this is __data override")
+
+	# def print__data(self):
+	# 	print("this is __data override")
+
+
+# class2 = NewAClass(["test3","test4"], {"key2":"value3", "key3":"value4"})
+
+# class2.print_data()
+
+# class2.print__data()
+print("*"*50+"New Class"+"*"*50)
+
+class MyClass:
+
+	def __init__(self, *args, **kwargs):
+		self.dataA = args
+		self.dataB = kwargs
+
+	def print_data(self):
+		print(self.dataA)
+
+	def print__data(self):
+		print(self.dataB)
+
+classA = MyClass(["data0", "data1", "data2"], **{"key0":"val0","key1":"val1","key2":"val2"})
+
+classA.print_data()
+
+classA.print__data()
+
+print("*"*50+"New Class"+"*"*50)
+
+class NewClass(MyClass):
+
+	def print_data(self):
+		print("this is _data override")
+
+	def print__data(self):
+		super(NewClass, self).print__data()
+		print("this is __data override")
+
+classB = NewClass(["data3", "data4", "data5"], **{"key3":"val3","key4":"val4","key5":"val5"})
+
+classB.print_data()
+
+classB.print__data()
+
+print("*"*50+"New Class"+"*"*50)
+
+class NewClassA:
+
+	def __init__(self, *args, **kwargs):
+		self._data = args
+		self.__data = kwargs
+
+
+	def print_data(self):
+		for _data in self._data:
+			print(_data)
+
+	def print__data(self):
+		for _key, _val in self.__data.items():
+			print("{} {}".format(_key, _val))
+
+
+classC = NewClassA(["data3", "data4", "data5"], **{"key3":"val3","key4":"val4","key5":"val5"})
+
+classC.print_data()
+
+classC.print__data()
+
+print("*"*50+"New Class"+"*"*50)
+
+class NewClassA(MyClass):
+
+	def print_data(self):
+		for _data in self.dataA:
+			print(_data)
+
+	def print__data(self):
+		for _key, _val in self.dataB.items():
+			print("{} {}".format(_key, _val))
+
+
+classD = NewClassA(["data3", "data4", "data5"], **{"key3":"val3","key4":"val4","key5":"val5"})
+
+classD.print_data()
+
+classD.print__data()
